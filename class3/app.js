d79fed95874a78b5442e7165642dbb6b2c3401d4
= <umrabadar555@gmail.com>
2024-05-14 17:50:35 +0500

    9 chap done

--- class3/app.js
+++ class3/app.js
@@ -1,39 +1,44 @@
 // 2 types of increments 1) post inc 2) pre inc
 var a = 2;
-var b = 10;
+// var b = 10;
 // a++ = a= a+1
-// document.write(++b);
-// editing
-
+// a-- = a= a-1
+// document.write(++b); 
 
 //document.write(a++ + ++a + a); // 2 + 4 + 4
 // document.write(++a + a++ + a)//3 + 3 + 4 =  10
 
-document.write(--b + b-- + b); 
-//document.write(a++ + --a + ++b - b++)
+//document.write(--b + b-- + b); //9 + 9 + 8 = 26
+// document.write(a++ + --a + ++b - b++) // 2 + 2 + 11 - 11
+
+// DMAS Rule    /  *  +  -
+
+// var totalCost = 1 + 3 * 4;  //16   13
+// document.write(totalCost)
+
+// var totalCost = 1 + (3 * 4); //16 13
+
+// var totalCost = (1 + 3) * 4; //16 
+// document.write(totalCost)
 
 
+// var resultOfComputation = (2 * 4) * 4 + 2;//34 
+// resultOfComputation = (2 * 4) * (4 + 2);//48 64 
+
+// document.write(resultOfComputation)
+
+// var userName = prompt("What is your Name?") // Came case
+// //alert("Thanks, " + userName + "!");
+
+// var message = "Thanks, for visiting ";
+// var banger = "!";
+// // alert(message + userName + banger);
+
+// var concat = message + userName + banger
+// alert(concat);
+ 
+//alert( 2  - 2 + 2 + "  2 plus 2 equals " );
 
 
-
-
-
-// 2 + 2 + 11 - 11 =4
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// document.write("Umra" + a)
-// document.write("Umra" - a)
+//document.write("Umra " + a)
+document.write("4" - 2)
