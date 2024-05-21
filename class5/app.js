479fcf9b471e1f3648b8528c47401b11ae60b3bb
= <umrabadar555@gmail.com>
2024-05-21 17:32:49 +0500

    13 chap done

---
+++ class5/app.js
@@ -0,0 +1,68 @@
+// var userName = prompt("What is your name?", "Female");
+
+// var cityOfLights = prompt("Enter the name of city of lights");
+// var capital = prompt("Enter capital of Pakistan")
+// if(cityOfLights === "karachi"){
+//     alert("Correct")
+// }
+// if(cityOfLights !== "karachi"){
+//     alert("Wrong Answer")
+// }
+
+// if(cityOfLights === "karachi" && capital === "islamabad"){
+//     alert("Correct")
+// }else{
+//     alert("Wrong Answer")
+// }
+
+// if(cityOfLights === "karachi" || cityOfLights === "Karachi"){
+//     alert("Correct")
+// }else{
+//     alert("Wrong Answer")
+// }
+// document.write("<h1> Number Guessing Game </h1>");
+
+
+// var num = +prompt("Guess the secret number: ")
+// var secNum = 8;
+
+// if(num==secNum){
+//     document.write("You win!")
+// }else if(num== 9){
+//     document.write("Aas pas ho, Zada aagy nai brho")
+// }else if(num==7){
+//     document.write("Aas pas ho, aagy brhao")
+// }else{
+//     document.write("Jao aagy brho")
+// }
+
+
+//Falsy Values
+
+// 0
+// undefined
+// null
+// false
+
+// if(1){
+//     document.write("Chal gya")
+// }else{
+//     document.write("You passed a Falsy value")
+// }
+
+var rishta = prompt("Kesa rishta h?")
+var age = +prompt("Umar kitni h?")
+
+if((rishta == "ameer" || rishta == "paisy wala") && age <=28){
+    document.write("Qabool h")
+}else if((rishta == "ghareeb" || rishta == "loyal") && age <=25){
+    document.write("Guzara kr lengy 5 saal ka time do")
+}else{
+    document.write("Istikhary mn na aagai")
+}
+
+
+
+
+
+
