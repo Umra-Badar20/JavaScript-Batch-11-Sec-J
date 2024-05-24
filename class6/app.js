edc170ecba92d07895474205359a55c5085a4e4b
= <umrabadar555@gmail.com>
2024-05-24 17:46:19 +0500

    16 chap done

--- class6/app.js
+++ class6/app.js
@@ -34,15 +34,41 @@
 // }
 
 
-var pass = "password.123"
-var userPass = prompt("Enter your password")
+// var pass = "password.123";
+// var userPass = prompt("Enter your password");
  
-if(userPass===""){
-    alert("Please enter your password") 
-}else{
-if(userPass===pass){
-alert("“Correct! The password you entered matches the original password")
-}else{
-alert("“Incorrect password”")
-}
-}
+// if(userPass===""){
+//     alert("Please enter your password") 
+// }else{
+// if(userPass===pass){
+// alert("“Correct! The password you entered matches the original password")
+// }else{
+// alert("“Incorrect password”")
+// }
+// }
+
+var city0 = "quetta"
+var city1 = "khi";
+var city2 = "lhr"
+var city3 ="isl"
+var city4 ="hyd"
+var city5 ="psh"
+var city6 ="fsl"
+var city7 ="mre"
+var city8 ="ksh"
+var city9 ="mlt"
+
+var city = ["quetta", "khi", "lhr" , "isl" , "hyd" , "psh" , "fsl" , "mre" ,"ksh" , "mlt"]
+// document.write("Welcome to " + city[6])
+
+var mixPlate = [3 , false, "Umra"]
+console.log(mixPlate)
+
+// city[10] = "skr"
+// city[1] = "abt"
+
+city.push("skr", "sial")
+//  document.write(city)
+
+ city.pop()
+ document.write(city)
