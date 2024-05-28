418a67d188e4137319e3a8463e85bb764013bdd7
= <umrabadar555@gmail.com>
2024-05-28 17:49:22 +0500

    17 chap done

---
+++ class7/app.js
@@ -0,0 +1,41 @@
+//  var grocery = ["aata" , "namak" , "dahi", "ghee" , "chicken" , "oil"]
+// var objNot = new Array()
+// objNot.push("Keyboard", "mouse")
+// console.log(objNot)
+
+// var sisGrocery = grocery.slice(2,3)
+// document.write(sisGrocery, "<br>")
+// console.log(grocery)
+// // document.write(grocery.length)
+
+
+
+
+
+
+
+
+// // grocery.pop()
+// // grocery.push("chawal")
+
+// // grocery.shift()
+// // grocery.unshift("chawal")
+// grocery.splice(0 , 1)
+// document.write(grocery)
+
+
+// var manufacturers = ["Apple", "Samsung", "Motrolla" , "Nokia"];
+// document.write('<select>')
+// document.write('<option value = "Apple">' +manufacturers[0] + '</option>')
+// document.write('<option value = "' +manufacturers[1] + '">' + manufacturers[1] + '</option>' )
+
+// document.write('</select>')
+
+
+
+
+// var devices = ["keyboard" , "mouse", "monitor"]
+// var out= devices.shift()
+// document.write(out)
+// var join = devices.join(" ")
+// document.write(join)
