b28b95db986c63af2909db39fcde001aa78d7267
= <umrabadar555@gmail.com>
2024-06-07 17:59:48 +0500

    20 chap done

---
+++ class9/app.js
@@ -0,0 +1,61 @@
+// for(var i=1; i<=5; i++){
+//    for(var j=1; j<=5; j++){
+//    document.write("*")
+//    }
+//    document.write("<br>")
+// }
+
+// for(var i=1; i<=5; i++){
+//     for(var j=1; j<=i; j++){
+//         document.write("* ")
+//     }
+//     document.write("<br>")
+// }
+ 
+// var rows= +prompt("Enter tye number of rows")
+
+// for(var i=1; i<=rows; i++){
+//     for(var j=rows; j>=i; j--){
+//         document.write("* ")
+//     }
+//     document.write("<br>")
+// }
+
+// for(var i=1; i<=5; i++){
+//     for(var j=5; 2>=3 ;j--){
+//         document.write("* ")
+//     }
+//     document.write("<br>")
+// }
+                         //0            1
+// var multidimension = [["Umra", 10], ["Sana", 11],["Sara",12]]
+// //2
+// document.write(multidimension[2][1])
+
+// var stdName= ["Sana", "Sara","Zara"]
+// var fName = ["Ali","khan",]
+
+// for(i=0; i<stdName.length; i++){
+//     for(j=0; j<fName.length; j++){
+//         document.write(stdName[i]+" "+ fName[j] +"<br>")
+
+//     }
+// }
+
+//i++ =i+1
+
+// for(var i=1; i<=100; i=i+10){
+//     for(var j=i; j <i+10 ; j++){
+//         document.write(j+ " ")
+//     }
+//     document.write("<br>")
+// }
+
+
+
+
+
+
+
+
+
