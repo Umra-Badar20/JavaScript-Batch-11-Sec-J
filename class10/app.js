3f341812af74f49a5cd0c9028f1e8cd558bed946
= <umrabadar555@gmail.com>
2024-06-11 17:54:03 +0500

    revision

---
+++ class10/app.js
@@ -0,0 +1,7 @@
+var array=[[1,2,3],[4,5,6],[7,8,9]];
+for (var i=0;i<array.length;i++){
+    for (var j=0;j<array.length[i];j++){
+       document.write(array[i][j]) 
+    }
+    document.write("<br>");
+}
