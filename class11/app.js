54355bc0113cbe424e332c01fecf97face5de697
= <umrabadar555@gmail.com>
2024-06-14 17:08:36 +0500

    24 chap done

---
+++ class11/app.js
@@ -0,0 +1,38 @@
+// var myName = "UmRa"
+// document.write(myName.toLowerCase())
+
+// var cities = ["Karachi", "Lahore", "Islamabad", "Hyderabad"]
+
+// var userCity = prompt("Enter your city name:")
+
+// var pro1= userCity.slice(0,1).toUpperCase()
+// var pro2= userCity.slice(1).toLowerCase()
+// var userCity = pro1+pro2
+// var flag = false
+// // document.write(userCity)
+
+// for(var i=0; i<cities.length; i++){
+//     if(userCity === cities[i]){
+//         flag=true
+//         alert(userCity+ " is one of the cleanest cities.")
+//     }
+// }
+// if(flag==false){
+//     alert(userCity+ " is not the cleanest city")
+// }
+
+
+
+
+
+
+// var students = ["Sana", "Sara", "Saba", "Zara","Tara", "Sana"]
+// // var myName = "Umra"
+// document.write(students.lastIndexOf("Sana"))
+
+var str = "Is this World War II Is?";
+// document.write(str.indexOf("Is"));
+// document.write(str.lastIndexOf("Is"));
+// document.write(str.charAt(10));
+
+
