ac3d09c822d92555934fa09af383591eb48c3ce8
= <umrabadar555@gmail.com>
2024-07-30 18:02:17 +0500

    49 chap done

---
+++ class21/app.js
@@ -0,0 +1,53 @@
+// function abc(){
+//     console.log(event.key)
+//     if(event.key === 13){
+//         alert("submit")
+//     }
+// }
+
+// console.log(document.getElementById('email'))
+
+// console.log(document.getElementsByTagName('li'))
+
+// console.log(document.getElementsByClassName('list'))
+
+
+
+// function checkAddress(fieldId) {
+//     if (document.getElementById(fieldId).value === "") {
+//         alert("Email address required.");
+//     }
+// }
+
+// function checkAddress(fieldId) {
+//     var val = document.getElementById(fieldId).value;
+//     if(val === "") {
+//     alert("Email address required.");
+//  }
+//  alert(val)
+// }
+
+// console.log(document.getElementById('para').innerHTML)
+
+
+
+
+
+// var counter = 0;
+
+// function increase(){
+//     counter++
+//     // console.log(counter)
+//     document.getElementById('counter').innerHTML = counter
+// }
+// function decrease(){
+//     counter--
+//     document.getElementById('counter').innerHTML = counter
+// }
+
+
+
+// console.log(document.getElementById('a').parentNode.parentNode)
+
+
+// console.log(document.getElementById('list').childNodes[1].childNodes[1])
