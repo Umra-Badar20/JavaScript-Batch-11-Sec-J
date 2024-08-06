8986fff19578249cc088da57110970f5b3500c2c
= <umrabadar555@gmail.com>
2024-08-06 17:48:07 +0500

    51 chap done

---
+++ class23/app.js
@@ -0,0 +1,4 @@
+function expand(){
+    var para = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates eveniet obcaecati consequatur aut ducimus voluptas debitis praesentium doloremque quas dolores rerum eius cumque, quia provident aspernatur, placeat libero impedit molestiae."
+    document.getElementById("para").innerHTML = para
+}
