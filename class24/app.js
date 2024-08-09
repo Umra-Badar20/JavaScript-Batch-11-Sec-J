7753df10cf22fa07355c6251d4635a77afc1afb1
= <umrabadar555@gmail.com>
2024-08-09 17:50:07 +0500

    54 chap done

---
+++ class24/app.js
@@ -0,0 +1,65 @@
+// function expand(){
+//     var link = document.getElementById("link")
+//     var moreText = document.getElementById("moreText")
+
+//     if(link.innerHTML==="Show more"){
+//         link.innerHTML ="Show less"
+//         moreText.style.display = "inline"
+//     }else{
+//           link.innerHTML ="Show more"
+//         moreText.style.display = "none"
+//     }
+// }
+
+
+
+
+// function submit(){
+//    var test = document.getElementById("test")
+//     console.log(test.value)
+
+//     test.value = ""
+
+
+
+// var radio = document.getElementsByName("gender")
+// console.log(radio)
+
+
+
+// for(var i = 0; i< radio.length; i++){
+//     if(radio[i].checked){
+//         console.log(radio[i].value)
+//     }
+// }
+// }
+
+
+
+
+// function invisible(){
+//     // document.getElementById("car").className= "hidden"
+//     document.getElementById("car").className += " zoom"
+// }
+
+
+// function link(){
+//     var links =document.getElementById("links")
+//     var address = links.href
+//     console.log(address)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
