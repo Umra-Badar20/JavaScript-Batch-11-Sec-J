b3c02d11ebdb43464065452beb49f3f121941b98
= <umrabadar555@gmail.com>
2024-08-13 17:33:02 +0500

    60 chap done

---
+++ class25/app.js
@@ -0,0 +1,30 @@
+// var par = document.getElementsByTagName("p");
+// var textInMiddleParagraph = par[1].innerHTML;
+// par[1].innerHTML = "This SUV is too big.";
+
+
+// for(var i =0; i<par.length;i++){
+//     par[i].style.color ="pink"
+// }
+
+// var t = document.getElementById("table");
+// var cells = t.getElementsByTagName("td");
+// for (var i = 0; i < cells.length; i++) {
+//     cells[i].style.backgroundColor = "hsl(0, 20%, 50%)";
+// }
+
+// console.log(document.childNodes[1].childNodes[2].childNodes[3])
+
+// console.log(document.childNodes[1].childNodes[1].childNodes[1].parentNode.parentNode.parentNode);
+
+//__________________form_______________________________
+
+document.getElementById("studentForm").addEventListener('submit',function(e){
+e.preventDefault()
+var name = document.getElementById("name").value
+var age = document.getElementById("age").value
+var className = document.getElementById("class").value
+
+
+
+})
