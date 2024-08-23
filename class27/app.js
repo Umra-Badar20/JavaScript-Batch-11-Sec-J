9c05ec93ddbc9ca00870f08ad74f1a7ec9d493cc
= <umrabadar555@gmail.com>
2024-08-23 17:47:37 +0500

    Post-App done

--- class27/app.js
+++ class27/app.js
@@ -4,20 +4,19 @@
 // console.log("my name is"+" "+fname+" "+ lname)
 // //Templete String
 // console.log(`my name is ${fname} ${lname}`)
-
-
-
+var backgroundImg;
 function post(){
     var title = document.getElementById("title")
     var description  = document.getElementById("description")
-
+    
+if(title.value.trim() && description.value.trim()){
     var post = document.getElementById("post")
     post.innerHTML+=`
-     <div class="card p-2">
+     <div class="card p-2 mb-2">
                     <div class="card-header">
                       <em>@post</em>
                     </div>
-                    <div class="card-body">
+                    <div style="background-image: url(${backgroundImg})" class="card-body">
                       <blockquote class="blockquote mb-0">
                         <p>${title.value}</p>
                         <footer class="blockquote-footer">${description.value}</footer>
@@ -27,4 +26,21 @@ function post(){
     `
     title.value =""
     description.value = ""
+  }else{
+    Swal.fire({
+      title: "Empty Post",
+      text: "Can't publish post without Title or Description",
+      icon: "question"
+    });  }
+}
+function selectImg(src){
+  backgroundImg =src
+  var bgImg = document.getElementsByClassName('bg-img')
+  // for(var i=0; i<bgImg.length; i++){
+  //   bgImg[i].classList.remove('selectedImg')
+  // } 
+  for(var i=0; i<bgImg.length; i++){
+    bgImg[i].className ="bg-img"
+  } 
+  event.target.className +=" selectedImg"
 }
