6ac7754f8eb306effaed99423e4f7bf4c43e6ef1
= <umrabadar555@gmail.com>
2024-09-20 17:56:37 +0500

    89 chap done

---
+++ class33/app.js
@@ -0,0 +1,28 @@
+// console.log(window.location);
+// // window.location.href = "./about.html"
+// window.location.href = "https://www.google.co.uk/"
+
+// var pass = document.getElementById('pass');
+// var pass1 = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/
+// if(!pass1.test(pass.value))
+// {
+//     alert("Enter Valid Passwoard")
+// }
+// console.log(pass.value)
+try{
+    aler("hello")
+    
+}catch(err){
+    console.log(err);  
+}
+console.log("After error");
+
+"Umra".startsWith("U")
+
+"Umra".startsWith("Um")
+
+"Umra".startsWith("Umz")
+
+isNaN("a")
+ 
+isNaN(1)
