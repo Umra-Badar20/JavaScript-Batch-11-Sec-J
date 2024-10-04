526b088f4b17276d961e0917b6cd73ea2465609b
Umra-Badar20 <umrabadar555@gmail.com>
2024-10-04 17:33:18 +0500

    Quill RTL +Browser revision

---
+++ class37/Quill RTE/app.js
@@ -0,0 +1,37 @@
+const toolbarOptions = [
+    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    ['blockquote', 'code-block'],
+    ['link', 'image', 'video', 'formula'],
+  
+    [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+    [{ 'list': 'ordered'}, { 'list': 'bullet' }, { 'list': 'check' }],
+    [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+    [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+    [{ 'direction': 'rtl' }],                         // text direction
+  
+    [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+  
+    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+    [{ 'font': [] }],
+    [{ 'align': [] }],
+  
+    ['clean']                                         // remove formatting button
+  ];
+  
+  const quill = new Quill('#editor', {
+    modules: {
+      toolbar: toolbarOptions
+    },
+    theme: 'snow',
+    placeholder : 'Write Something here'
+  });
+
+  function submit(){
+    const delta = quill.getContents();
+    console.log(delta)
+    console.log(quill.root.innerHTML)
+    var content = document.getElementById("content")
+    content.innerHTML = quill.root.innerHTML
+
+  }
